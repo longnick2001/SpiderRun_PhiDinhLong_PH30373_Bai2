9e4827ebead5b1737bd38f1d2d82f46859488b08
longnick2001 <129032558+longnick2001@users.noreply.github.com>
2023-11-08 18:14:51 +0700

    lab2

---
+++ Assets/Scripts/Scroll_Mapping.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll_Mapping : MonoBehaviour
+{
+    public float ScrollSpeed = 0.5f;
+
+    public float offSet;
+
+    public Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offSet += Time.deltaTime * ScrollSpeed;
+        renderer.material.mainTextureOffset = new Vector2(offSet, 0.01f);
+    }
+}
